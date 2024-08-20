76aed4a1603f1a25135a63326be10ffaae97d637
Florian Westphal <fw@strlen.de>
2024-08-21 00:12:27 +0200

    src: mnl: always dump all netdev hooks if no interface name was given
    commit db70959a5ccf2952b218f51c3d529e186a5a43bb upstream.
    
    Instead of not returning any results for
      nft list hooks netdev
    
    Iterate all interfaces and then query all of them.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- include/iface.h
+++ include/iface.h
@@ -2,6 +2,8 @@
 #define _NFTABLES_IFACE_H_
 
 #include <net/if.h>
+#include <list.h>
+#include <stdint.h>
 
 struct iface {
 	struct list_head	list;
@@ -15,4 +17,5 @@ char *nft_if_indextoname(unsigned int ifindex, char *name);
 void iface_cache_update(void);
 void iface_cache_release(void);
 
+const struct iface *iface_cache_get_next_entry(const struct iface *prev);
 #endif
