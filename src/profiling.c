1g4-mirror/nftables

commit 868040f8922382925e75bf3579f66cfcd2f080e7

configure: Implement --enable-profiling option
This will set compiler flag --coverage so code coverage may be inspected
using gcov.

In order to successfully profile processes which are killed or
interrupted as well, add a signal handler for those cases which calls
exit(). This is relevant for test cases invoking nft monitor.

Signed-off-by: Phil Sutter <phil@nwl.cc>

src/profiling.c
@@ -1,0 +1,51 @@
+/*
+ * Copyright (c) Red Hat GmbH.  Author: Phil Sutter <phil@nwl.cc>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 (or any
+ * later) as published by the Free Software Foundation.
+ */
+
+#include <nft.h>
+#include <profiling.h>
+
+#include <sys/signalfd.h>
+#include <signal.h>
+#include <stdio.h>
+
+int get_signalfd(void)
+{
+	sigset_t mask;
+	int fd;
+
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGTERM);
+	sigaddset(&mask, SIGINT);
+
+	fd = signalfd(-1, &mask, 0);
+	if (fd < 0) {
+		perror("signalfd()");
+		return fd;
+	}
+	if (sigprocmask(SIG_BLOCK, &mask, NULL) < 0) {
+		perror("sigprocmask()");
+		close(fd);
+		return -1;
+	}
+	return fd;
+}
+
+void check_signalfd(int fd)
+{
+	struct signalfd_siginfo info;
+
+	if (read(fd, &info, sizeof(info)) < (signed)sizeof(info))
+		return;
+
+	switch (info.ssi_signo) {
+	case SIGTERM:
+		exit(143);
+	case SIGINT:
+		exit(130);
+	}
+}

.gitignore
@@ -19,6 +19,11 @@ nftversion.h
 # cscope files
 /cscope.*
 
+# gcov-related
+*.gcda
+*.gcno
+*.gcov
+
 # Generated by tests
 *.payload.got
 tests/build/tests.log

Makefile.am
@@ -97,6 +97,7 @@ noinst_HEADERS = \
 	include/owner.h \
 	include/parser.h \
 	include/payload.h \
+	include/profiling.h \
 	include/proto.h \
 	include/rt.h \
 	include/rule.h \
@@ -163,6 +164,10 @@ AM_CFLAGS = \
 	$(NULL)
 
 AM_YFLAGS = -d -Wno-yacc
+
+if BUILD_PROFILING
+AM_CFLAGS += --coverage
+endif
 
 ###############################################################################
 
@@ -263,6 +268,10 @@ src_libnftables_la_SOURCES += \
 	src/json.c \
 	src/parser_json.c \
 	$(NULL)
+endif
+
+if BUILD_PROFILING
+src_libnftables_la_SOURCES += src/profiling.c
 endif
 
 src_libnftables_la_LDFLAGS = \
@@ -454,3 +463,15 @@ TESTS = tests/build/run-tests.sh \
 	tests/py/nft-test.py \
 	tests/shell/run-tests.sh
 endif
+
+all_c_sources = $(filter %.c,$(src_libnftables_la_SOURCES)) $(src_nft_SOURCES)
+if BUILD_MINIGMP
+all_c_sources += $(src_libminigmp_la_SOURCES)
+endif
+if BUILD_AFL
+all_c_sources += $(tools_nft_afl_SOURCES)
+endif
+CLEANFILES += src/libparser_la-parser_bison.gcno
+CLEANFILES += src/libparser_la-scanner.gcno
+CLEANFILES += $(all_c_sources:.c=.gcno)
+CLEANFILES += $(src_nft_SOURCES:.c=.gcda)

configure.ac
@@ -156,6 +156,13 @@ AC_ARG_ENABLE([distcheck],
 	      [enable_distcheck=yes], [])
 AM_CONDITIONAL([BUILD_DISTCHECK], [test "x$enable_distcheck" = "xyes"])
 
+AC_ARG_ENABLE([profiling],
+	      AS_HELP_STRING([--enable-profiling], [build for use of gcov/gprof]),
+	      [enable_profiling="$enableval"], [enable_profiling="no"])
+AM_CONDITIONAL([BUILD_PROFILING], [test "x$enable_profiling" = xyes])
+AM_COND_IF([BUILD_PROFILING],
+	   [AC_DEFINE([BUILD_PROFILING], [1], [Define for profiling])])
+
 AC_CONFIG_FILES([					\
 		Makefile				\
 		libnftables.pc				\
@@ -170,7 +177,8 @@ nft configuration:
   use mini-gmp:			${with_mini_gmp}
   enable man page:              ${enable_man_doc}
   libxtables support:		${with_xtables}
-  json output support:          ${with_json}"
+  json output support:          ${with_json}
+  collect profiling data:       ${enable_profiling}"
 
 if test "x$unitdir" != "x"; then
 AC_SUBST([unitdir])

include/profiling.h
@@ -1,0 +1,12 @@
+#ifndef NFTABLES_PROFILING_H
+#define NFTABLES_PROFILING_H
+
+#ifdef BUILD_PROFILING
+int get_signalfd(void);
+void check_signalfd(int fd);
+#else
+static inline int get_signalfd(void) { return -1; }
+static inline void check_signalfd(int fd) { /* empty */ }
+#endif
+
+#endif /* NFTABLES_PROFILING_H */

src/mnl.c
@@ -39,6 +39,7 @@
 #include <errno.h>
 #include <utils.h>
 #include <nftables.h>
+#include <profiling.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_arp.h>
 
@@ -2390,6 +2391,7 @@ int mnl_nft_event_listener(struct mnl_socket *nf_sock, unsigned int debug_mask,
 	unsigned int bufsiz = NFTABLES_NLEVENT_BUFSIZ;
 	int fd = mnl_socket_get_fd(nf_sock);
 	char buf[NFT_NLMSG_MAXSIZE];
+	int sigfd = get_signalfd();
 	fd_set readfds;
 	int ret;
 
@@ -2401,10 +2403,15 @@ int mnl_nft_event_listener(struct mnl_socket *nf_sock, unsigned int debug_mask,
 	while (1) {
 		FD_ZERO(&readfds);
 		FD_SET(fd, &readfds);
+		if (sigfd != -1)
+			FD_SET(sigfd, &readfds);
 
-		ret = select(fd + 1, &readfds, NULL, NULL, NULL);
+		ret = select(max(fd, sigfd) + 1, &readfds, NULL, NULL, NULL);
 		if (ret < 0)
 			return -1;
+
+		if (FD_ISSET(sigfd, &readfds))
+			check_signalfd(sigfd);
 
 		if (FD_ISSET(fd, &readfds)) {
 			ret = mnl_socket_recvfrom(nf_sock, buf, sizeof(buf));

commit 3e93d84755c55ac1b72137a487e5c6b2932f127d

profiling: Include unistd.h to avoid compiler warnings
RHEL8's gcc-8.5.0 emits these warnings:

src/profiling.c: In function ‘get_signalfd’:
src/profiling.c:32:3: warning: implicit declaration of function ‘close’; did you mean ‘pclose’? [-Wimplicit-function-declaration]
   close(fd);
   ^~~~~
   pclose
src/profiling.c: In function ‘check_signalfd’:
src/profiling.c:42:6: warning: implicit declaration of function ‘read’; did you mean ‘fread’? [-Wimplicit-function-declaration]
  if (read(fd, &info, sizeof(info)) < (signed)sizeof(info))
      ^~~~
      fread

Fixes: 868040f892238 ("configure: Implement --enable-profiling option")
Signed-off-by: Phil Sutter <phil@nwl.cc>

src/profiling.c
@@ -12,6 +12,7 @@
 #include <sys/signalfd.h>
 #include <signal.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int get_signalfd(void)
 {